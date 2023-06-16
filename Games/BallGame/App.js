edb883bc0f946a4726fcfed3daa6b7f515a34f3d
Patrick <psn1462@gmail.com>
2023-06-16 16:20:09 -0300

    feat: changes
    changes

--- Games/BallGame/App.js
+++ Games/BallGame/App.js
@@ -1,115 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, StyleSheet, Animated, Dimensions } from 'react-native';
+import Constants from 'expo-constants';
 
-const FIELD_WIDTH = 10;
-const FIELD_HEIGHT = 10;
-const EMPTY = ' ';
-const BALL = 'O';
-const VERTICAL_CANNON = '|';
-const HORIZONTAL_CANNON = '-';
-const BEAM = 'x';
-const MAX_LIVES = 5;
-const MAX_TIME = 30;
-
-const App = () => {
-  const [field, setField] = useState([]);
-  const [lives, setLives] = useState(MAX_LIVES);
-  const [time, setTime] = useState(MAX_TIME);
-  const [cannon, setCannon] = useState({ x: Math.floor(FIELD_WIDTH / 2), y: Math.floor(FIELD_HEIGHT / 2) });
-  const [balls, setBalls] = useState([]);
+export default function App() {
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const speed = useRef(5).current;
 
   useEffect(() => {
-    initializeField();
-    const gameInterval = setInterval(() => {
-      updateField();
-      moveBalls();
-      setTime((prevTime) => prevTime - 1);
-    }, 1000);
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height - Constants.statusBarHeight;
+    const ballSize = 30;
 
-    return () => clearInterval(gameInterval);
+    const moveBall = () => {
+      const moveX = Animated.timing(position, {
+        toValue: { x: screenWidth - ballSize, y: screenHeight - ballSize },
+        duration: (screenWidth - ballSize) / speed,
+        useNativeDriver: false,
+      });
+
+      const moveY = Animated.timing(position, {
+        toValue: { x: 0, y: 0 },
+        duration: (screenHeight - ballSize) / speed,
+        useNativeDriver: false,
+      });
+
+      Animated.sequence([moveX, moveY, moveBall()]).start();
+    };
+
+    moveBall();
+
+    return () => {
+      position.stopAnimation();
+      position.setValue({ x: 0, y: 0 });
+    };
   }, []);
 
-  const initializeField = () => {
-    const newField = [];
-    for (let i = 0; i < FIELD_HEIGHT; i++) {
-      newField[i] = [];
-      for (let j = 0; j < FIELD_WIDTH; j++) {
-        if (i === 0 || i === FIELD_HEIGHT - 1 || j === 0 || j === FIELD_WIDTH - 1) {
-          newField[i][j] = '#'; // boundary walls
-        } else {
-          newField[i][j] = EMPTY;
-        }
-      }
-    }
-    setField(newField);
-  };
-
-  const updateField = () => {
-    const newField = [...field];
-    for (let i = 0; i < FIELD_HEIGHT; i++) {
-      for (let j = 0; j < FIELD_WIDTH; j++) {
-        if (newField[i][j] === BEAM) {
-          if (i > 0 && newField[i - 1][j] === BALL) {
-            newField[i - 1][j] = EMPTY;
-            setLives((prevLives) => prevLives - 1);
-          } else {
-            newField[i][j] = EMPTY;
-            if (i > 0) {
-              newField[i - 1][j] = BEAM;
-            }
-          }
-        }
-      }
-    }
-
-    for (const ball of balls) {
-      newField[ball.y][ball.x] = BALL;
-    }
-
-    newField[cannon.y][cannon.x] =
-      cannon.x === 0 || cannon.x === FIELD_WIDTH - 1 ? VERTICAL_CANNON : HORIZONTAL_CANNON;
-
-    setField(newField);
-  };
-
-  const moveBalls = () => {
-    const newBalls = [];
-    for (const ball of balls) {
-      let dx = ball.x < cannon.x ? 1 : -1;
-      let dy = ball.y < cannon.y ? 1 : -1;
-      let x = ball.x + dx;
-      let y = ball.y + dy;
-
-      if (x === 0 || x === FIELD_WIDTH - 1) {
-        dx *= -1;
-        x += dx;
-      }
-
-      if (y === 0 || y === FIELD_HEIGHT - 1) {
-        dy *= -1;
-        y += dy;
-      }
-
-      newBalls.push({ x, y });
-    }
-    setBalls(newBalls);
-  };
-
-  const isGameOver = () => {
-    return lives <= 0 || time <= 0;
-  };
-
   return (
-    <View>
-      <Text>Field:</Text>
-      {field.map((row, rowIndex) => (
-        <Text key={rowIndex}>{row.join('')}</Text>
-      ))}
-      <Text>Lives: {lives}</Text>
-      <Text>Time: {time}</Text>
-      {isGameOver() && <Text>Game Over!</Text>}
+    <View style={styles.container}>
+      <Animated.View style={[styles.ball, position.getLayout()]} />
     </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#006998',
+  },
+  ball: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'white',
+  },
+});
